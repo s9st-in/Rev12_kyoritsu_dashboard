d110ecb3c7686a3cde898ea7bc774d0e713bb8ae
dorichapi <38826817+dorichapi@users.noreply.github.com>
2025-05-28 15:49:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,11 @@ async function fetchSpecialData() {
        // ✅ タイトルを維持しながらデータを左詰めで表示　　*2025.5.28 「経営戦略室より」を追加
         // ✅ 『』を追加し、左詰めに設定
         document.getElementById("suiyokai-card").innerHTML = `<strong>『水曜会 Top Down!』</strong><br>${result.specialData.suiyokai || "データなし"}`;
-        document.getElementById("keiei-card").innerHTML = `<strong>『経営戦略室より』</strong><br>R8年度診療報酬改定に向けて議論がスタート<br>（急性期医療に関するテーマ）`;
+        document.getElementById("keiei-card").innerHTML = `<div style="font-weight:bold;">
+        『経営戦略室より』<br>
+        R8年度診療報酬改定に向けて議論がスタート<br>
+        （急性期医療に関するテーマ）</div>`;
+
 
         // ✅ 追加したカードのサイズを変更（横幅と高さを指定）
         document.getElementById("suiyokai-card").style.width = "700px";  // 横幅
